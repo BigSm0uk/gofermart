f617b488f8b74ffbb5c1b33dda09395a09d0fc26
Mihklz <mihkl137@gmail.com>
2025-10-15 14:46:28 +0300

    Удаление дублирующей функции MustMigrate

--- internal/app/gophermart/migrator.go
+++ internal/app/gophermart/migrator.go
@@ -7,18 +7,6 @@ import (
 	"github.com/pressly/goose/v3"
 )
 
-func MustMigrate(db *sql.DB) {
-	goose.SetBaseFS(migration.LoyaltyMigrations)
-
-	if err := goose.SetDialect("postgres"); err != nil {
-		panic(err)
-	}
-
-	if err := goose.Up(db, "loyalty"); err != nil {
-		panic(err)
-	}
-}
-
 func Migrate(db *sql.DB) error {
 	goose.SetBaseFS(migration.LoyaltyMigrations)
 
